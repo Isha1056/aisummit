350f35275e187f3cced83b87585b55ae17c770d6
Isha Gupta <ishagu10@gmail.com>
2022-02-27 04:01:40 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom";
 import logo from './logo.svg';
 import './App.css';
+import Footer from './Footer/Footer'
+import AISummitLogo from './img/AISummitLogo.png';
+import GDSCLogo from './img/GDSCMPSTME.png';
+
+const iframe = '<iframe src="https://i.simmer.io/@Oink/aisummit" style="width:100vw;height:100vh; position: relative;top: -11vh !important;z-index: 1;border: none !important;"></iframe>';
+
+function Iframe(props) {
+  return (<div className="framewrapper" dangerouslySetInnerHTML={{ __html: props.iframe ? props.iframe : "" }}/>);
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <div className="row waverow">
+          <svg className="wavebackground" width="100%" height="100%" id="svg" viewBox="0 35 1440 200" xmlns="http://www.w3.org/2000/svg" class="transition duration-300 ease-in-out delay-150"><defs><linearGradient id="gradient" x1="0%" y1="50%" x2="100%" y2="50%"><stop offset="5%" stop-color="#0693e3ff"></stop><stop offset="95%" stop-color="#9900efff"></stop></linearGradient></defs><path d="M 0,400 C 0,400 0,200 0,200 C 105.89285714285714,180.39285714285714 211.78571428571428,160.78571428571428 322,179 C 432.2142857142857,197.21428571428572 546.7500000000001,253.25 684,252 C 821.2499999999999,250.75 981.2142857142858,192.21428571428572 1111,174 C 1240.7857142857142,155.78571428571428 1340.392857142857,177.89285714285714 1440,200 C 1440,200 1440,400 1440,400 Z" stroke="none" stroke-width="0" fill="url(#gradient)" class="transition-all duration-300 ease-in-out delay-150 path-0" transform="rotate(-180 720 200)"></path></svg>
+          <img src={AISummitLogo} className="summitLogo" />
+          <img src={GDSCLogo} className="gdscLogo" />
+        </div>
+        <div className="row framerow">
+          <Iframe iframe={iframe}/>
+        </div>
+        <div className="row footerrow">
+          <Footer/>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Footer/Footer.js
@@ -0,0 +1,33 @@
+import React from "react";
+import './Footer.css'
+import GDSCLogo from '../img/GDSCMPSTME.png';
+
+export default function Footer() {
+    return (
+
+        <footer className="nb-footer">
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-12">
+                        <div className="about">
+                            <img src={GDSCLogo} className="img-responsive center-block GDSCLogo" alt=""/>
+                            <p>The data you provide us shall be used to facilitate your participation in our event as well as stored securely for further internal use. We do not indulge in the trading and barter of data. If you know any officer or executive of the GDSC who may be performing such tasks, please reach out to our Grievance officer to register a complaint.</p> 
+                            <p>Google Developer Student Club, NMIMS MPSTME Mumbai is a registered chapter of Google Developer Student Clubs, a program initiated by Google LLC and Google Developers.</p>
+                            <p>Designed and developed by Angad Singh Kataria</p>
+                            <div class ="social-media">
+                            {/* <ul class ="list-inline">
+                            <li><a href="https://www.linkedin.com/company/google-developer-student-club-mpstme/" title="" target="__blank"><i class ="fa fa-linkedin"/></a></li>
+                            <li><a href="https://www.instagram.com/gdsc.mpstme/" title="" target="__blank"><i class ="fa fa-instagram"/></a></li>
+                            <li><a href="https://github.com/GDSC-NMIMS-MPSTME-Mumbai" title="" target="__blank"><i class ="fa fa-github"/></a></li>
+                            <li><a href="https://gdsc.community.dev/mukesh-patel-school-of-technology-management-and-engineering-mumbai/" title="" target="__blank"><i class ="fa fa-google"/></a></li>
+                            </ul> */}
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </footer>
+
+    );
+}
