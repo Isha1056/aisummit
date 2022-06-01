d5ca25dd1f7f4928650b11ae43b9c7d8fa9fbd2a
Isha Gupta <ishagu10@gmail.com>
2022-06-01 14:46:12 +0530

    Update Footer.js

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -13,7 +13,7 @@ export default function Footer() {
                             <img src={GDSCLogo} className="img-responsive center-block GDSCLogo" alt=""/>
                             <p>The data you provide us shall be used to facilitate your participation in our event as well as stored securely for further internal use. We do not indulge in the trading and barter of data. If you know any officer or executive of the GDSC who may be performing such tasks, please reach out to our Grievance officer to register a complaint.</p> 
                             <p>Google Developer Student Club, NMIMS MPSTME Mumbai is a registered chapter of Google Developer Student Clubs, a program initiated by Google LLC and Google Developers.</p>
-                            <p>Designed and developed by Angad Singh Kataria</p>
+                            <p>Designed and developed by Angad Singh Kataria and Isha Gupta</p>
                             <div class ="social-media">
                             {/* <ul class ="list-inline">
                             <li><a href="https://www.linkedin.com/company/google-developer-student-club-mpstme/" title="" target="__blank"><i class ="fa fa-linkedin"/></a></li>
